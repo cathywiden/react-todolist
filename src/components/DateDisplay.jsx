1f93f660e84edc5bb36213cf574a42238161544b
Katalin Widén <katalinwiden@gmail.com>
2023-03-27 21:44:41 +0200

    auto formatting code

--- src/components/DateDisplay.jsx
+++ src/components/DateDisplay.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DateDisplay = () => {
   const currentDate = new Date().toLocaleDateString("en-GB");
-  
+
   return (
     <div className="date">
       <span>{currentDate}</span>
